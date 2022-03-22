09874c297f3dbe61aa67c1bf4daabef9eb3dce7c
Rahaf Alhabashi <rahafalhabashi@gmail.com>
2022-03-21 20:11:51 -0400

    commit tree 0acc13f296f1ee4b82f15e2556faac8f32b7a729

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+        console.log("The DOM has loaded")
+        const text = document.getElementById("text")
+        text.innerHTML = "This is really cool!"
+    // document.text.innerHTML = document.text.innerHTML.replace("JavaScript is so cool. It lets me add text to my page programmatically.","This is really cool!")
+})
+
+console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
